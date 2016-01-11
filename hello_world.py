8b77ec8bd5077b16024fbadd3e2b0125b809477f
bbouley <b.bouley@hotmail.co.uk>
2016-01-10 21:52:55 -0700

    first commit with len status and print commands

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print('Hello, World')
+
+phrase = 'Hello, World!'
+
+print(phrase)
+
+long_string = '''This is a long string that will\
+ end up taking up 2 lines'''
+
+print(long_string)
+
+long_string_length = len(long_string)
+
+print(len(long_string))
+
+print(long_string_length)
